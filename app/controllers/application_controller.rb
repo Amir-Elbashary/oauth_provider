3b610056d9572ceb43feee969cfdb7de37f62fa6
Amir El-Bashary <amir.elbashary@gmail.com>
2023-12-11 17:28:21 +0200

    add devise and doorkeeper with basic configs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    session[:previous_url] || home_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  use_doorkeeper
+  devise_for :users
+
+  get '/', to: 'home#index', as: :home
 end
